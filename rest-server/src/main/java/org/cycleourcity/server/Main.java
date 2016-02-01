a4f0150f1e11e675054a6459ed5c68235a6de95f
rodrigo lourenco <rodrigo.jm.lourenco@gmail.com>
2016-02-01 06:59:57 -0800

    modificacoes menores

--- rest-server/src/main/java/org/cycleourcity/server/Main.java
+++ rest-server/src/main/java/org/cycleourcity/server/Main.java
@@ -56,10 +56,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
     	
-        final HttpServer server = startServer();
+        
     	
-    	CycleOurCityManager.getInstance();
-    	CycleOurCitySecurityManager.getManager();
+    	//CycleOurCityManager.getInstance();
+    	//CycleOurCitySecurityManager.getManager();
+    	
+    	final HttpServer server = startServer();
     	
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
